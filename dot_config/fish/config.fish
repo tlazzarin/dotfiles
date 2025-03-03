a6a2330661773f61cee15de1749b4f9f8a55346a
Tommaso Lazzarin <36604344+tlazzarin@users.noreply.github.com>
2025-03-03 22:09:39 +0100

    Add fish shell (tmp)

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,33 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+#Alias
+alias c='clear'
+alias lzg='lazygit'
+alias l='ll'
+
+#Yazi
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+#Starship
+starship init fish | source
+
+#Add rust
+fish_add_path $HOME/.cargo/bin
+
+#GitHub Sign commit
+set -gx GPG_TTY (tty)
+
+# overwrite greeting
+function fish_greeting
+end
+
+function fish_user_key_bindings
+	bind ctrl-g 'lazygit' repaint
+end
